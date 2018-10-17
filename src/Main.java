1ccdef505e0d3a88709a92833d169a8ab07249f3
florian tjeertes <florianmac@MacBook-Pro-van-florian.local>
2018-10-17 10:20:22 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+
+import java.util.Scanner;
+
+
+public class Main {
+
+    public static void main(String[] args)
+
+    {
+
+
+        System.out.println("welcomeeeeee everyone dit word de test van de eeuw \n " +
+                           "--------------------------------------------------" );
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("hoe heet jij?");
+
+        String antwoord1 =scan.nextLine();
+        System.out.println("welkom "+ antwoord1 + "\n--------------------------------------------------");
+
+        System.out.println("hoe voel je je (vertel in 1 woord)");
+        String antwoord2 =scan.nextLine();
+        System.out.println("je voelt je dus, " + antwoord2  + " " + antwoord1 +"? \n --------------------------------------------------");
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
